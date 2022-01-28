2c37e9b3629620f6923d42bf6c8867cceb9238f0
Dessmondh <danilillo55@gmail.com>
2022-01-28 12:59:19 +0100

    login almost working + dependencies fix

---
+++ app/src/main/java/com/example/adae/login.kt
@@ -0,0 +1,136 @@
+package com.example.adae
+
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+
+class login : AppCompatActivity() {
+    private val GOOGLE_SIGN_IN = 100
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        checkSession()
+        supportActionBar?.hide()
+        val googleConf = GoogleSignInOptions.
+        Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).
+        requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail().
+            build()
+        val gClient = GoogleSignIn.getClient(this,
+            googleConf)
+        val registerButton : ImageButton = findViewById(R.id.buttonId);
+        registerButton.setOnClickListener{
+            gClient.signOut()
+            val intent = gClient.getSignInIntent()
+            startActivityForResult(intent, 100)
+        }
+
+
+
+    }
+//Login simple
+    fun onClickLoginNext(view: View) {
+        var email: EditText = findViewById(R.id.email2);
+        var passw: EditText = findViewById(R.id.editTextTextPassword2);
+        if (email.text.isNotEmpty() && passw.text.isNotEmpty()) {
+            try {
+                FirebaseAuth.getInstance()
+                    .signInWithEmailAndPassword(email.text.toString(), passw.text.toString())
+                    .addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            login()
+                        } else {
+                            //error
+                            ShowAlert2()
+                        }
+                    }
+
+            } catch (ex: Exception) {
+                ShowAlert2()
+            }
+
+        }
+
+    }
+private fun logout(){
+    var logout = Intent(this, login::class.java)
+    startActivity(logout)
+}
+    private fun login() {
+        var login = Intent(this, Menu::class.java)
+        startActivity(login)
+    }
+//En caso de error, se muestra un mensaje
+    private fun ShowAlert2() {
+        var builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Email & Password erróneos")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+//Comprueba si hay una sesión activa
+    private fun checkSession() {
+        val preferences =
+            getSharedPreferences(getString(R.string.preferences), Context.MODE_PRIVATE)
+        val email = preferences.getString("email", null);
+        if (email != null) {
+            logout()
+        }
+    }
+
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == GOOGLE_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)!!
+                if (account != null) {
+                    val credential = GoogleAuthProvider.getCredential(account.idToken, null)
+                    FirebaseAuth.getInstance().signInWithCredential(credential)
+                        .addOnCompleteListener {
+                            if (it.isSuccessful) {
+                                logout()
+                            } else {
+
+                            }
+
+
+                        }
+                }
+            } catch (e: ApiException) {
+
+            }
+        }
+    }
+
+    fun irRegistro(view: android.view.View){
+        var irRegistro = Intent (this, MainActivity::class.java).apply{
+
+        }
+        startActivity(irRegistro)
+    }
+fun irLoginMovil(view: android.view.View){
+    var irMovil = Intent(this, LoginMovil::class.java).apply{
+
+    }
+    startActivity(irMovil)
+}
+
+}
+
+
