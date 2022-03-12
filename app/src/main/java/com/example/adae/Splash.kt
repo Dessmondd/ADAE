565f93ce1cbd6e3d9fbd5b2baee577439b698800
Dessmondh <danilillo55@gmail.com>
2022-03-12 20:36:43 +0100

    music working

--- app/src/main/java/com/example/adae/Splash.kt
+++ app/src/main/java/com/example/adae/Splash.kt
@@ -1,6 +1,7 @@
 package com.example.adae
 
 import android.content.Intent
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
@@ -10,11 +11,21 @@ class Splash : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash)
         supportActionBar?.hide()
+        music()
         Handler().postDelayed({
             val intent = Intent(this, login::class.java)
             startActivity(intent)
             finish()
+            music()
         },3000)
     }
+
+
+    fun music(){
+        var mediaPlayer: MediaPlayer = MediaPlayer.create(this, R.raw.backgroundsound)
+        mediaPlayer.isLooping;
+        mediaPlayer.start()
+    }
+
 //hola
 }
