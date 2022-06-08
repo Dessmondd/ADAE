a7ad9dce9735afe6ee1f9a941e65f575d34525be
Dessmondh <danilillo55@gmail.com>
2022-06-08 17:33:20 +0200

    fix ruta

--- app/src/main/java/com/example/adae/PopUpEntrenadores/pop_up_blaine.kt
+++ app/src/main/java/com/example/adae/PopUpEntrenadores/pop_up_blaine.kt
@@ -1,17 +1,20 @@
 package com.example.adae.PopUpEntrenadores
 
 import android.content.pm.ActivityInfo
+import android.media.Image
 import android.os.Bundle
 import android.util.DisplayMetrics
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.adae.R
 import com.example.adae.databinding.ActivityColeecionBinding
+import java.util.*
 
 
 class pop_up_blaine : AppCompatActivity() {
     private lateinit var binding: ActivityColeecionBinding
     override fun onCreate(savedInstanceState: Bundle?) {
-
+        val language = Locale.getDefault().getDisplayLanguage().toString()
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
         setContentView(R.layout.activity_pop_up_blaine)
@@ -24,7 +27,13 @@ class pop_up_blaine : AppCompatActivity() {
         val height = dm.heightPixels
 
         window.setLayout((width * 0.8).toInt(), (height * 0.4).toInt())
-
+        val view = findViewById<ImageView>(R.id.erikacard)
+        if(language == "English"){
+            view.setImageResource(R.drawable.canelaeng)
+        }
+        if(language == "Deutsch"){
+            view.setImageResource(R.drawable.canelaaleman)
+        }
 
     }
 
