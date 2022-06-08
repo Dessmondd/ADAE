ccbc3abe5212cb3bfb4ca5e5991ba656d7298da7
Dessmondh <danilillo55@gmail.com>
2022-06-08 16:33:44 +0200

    fix ruta

---
+++ app/src/main/java/com/example/adae/PopUpEntrenadores/pop_up_erika.kt
@@ -0,0 +1,31 @@
+package com.example.adae.PopUpEntrenadores
+
+import android.content.pm.ActivityInfo
+import android.os.Bundle
+import android.util.DisplayMetrics
+import androidx.appcompat.app.AppCompatActivity
+import com.example.adae.R
+import com.example.adae.databinding.ActivityColeecionBinding
+
+
+class pop_up_erika : AppCompatActivity() {
+    private lateinit var binding: ActivityColeecionBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        supportActionBar?.hide()
+        setContentView(R.layout.activity_pop_up_koga)
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT)
+        binding = ActivityColeecionBinding.inflate(layoutInflater)
+        val dm = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(dm)
+
+        val width = dm.widthPixels
+        val height = dm.heightPixels
+
+        window.setLayout((width * 0.8).toInt(), (height * 0.4).toInt())
+
+
+    }
+
+}
